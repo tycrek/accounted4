371d733285ef5bde3e71eef80d632cc4a201f31c
tycrek <josh.moore@jmoore.dev>
2022-05-10 15:49:26 -0600

    Added proper types to Twitch provider

--- src/providers/Twitch.ts
+++ src/providers/Twitch.ts
@@ -3,7 +3,59 @@ import { Request, Response, NextFunction } from 'express';
 import { encodeObjectAsParams } from '../accounted4';
 import { Provider, ProviderOptions } from '../Provider';
 
+type TWITCH_SCOPES =
+	'analytics:read:extensions' |
+	'analytics:read:games' |
+	'bits:read' |
+	'channel:edit:commercial' |
+	'channel:manage:broadcast' |
+	'channel:manage:extensions' |
+	'channel:manage:polls' |
+	'channel:manage:predictions' |
+	'channel:manage:redemptions' |
+	'channel:manage:schedule' |
+	'channel:manage:videos' |
+	'channel:read:editors' |
+	'channel:read:goals' |
+	'channel:read:hype_train' |
+	'channel:read:polls' |
+	'channel:read:predictions' |
+	'channel:read:redemptions' |
+	'channel:read:stream_key' |
+	'channel:read:subscriptions' |
+	'clips:edit' |
+	'moderation:read' |
+	'moderator:manage:banned_users' |
+	'moderator:read:blocked_terms' |
+	'moderator:manage:blocked_terms' |
+	'moderator:manage:automod' |
+	'moderator:read:automod_settings' |
+	'moderator:manage:automod_settings' |
+	'moderator:read:chat_settings' |
+	'moderator:manage:chat_settings' |
+	'user:edit' |
+	'user:edit:follows' |
+	'user:manage:blocked_users' |
+	'user:read:blocked_users' |
+	'user:read:broadcast' |
+	'user:read:email' |
+	'user:read:follows' |
+	'user:read:subscriptions' |
+	'channel:moderate' |
+	'chat:edit' |
+	'chat:read' |
+	'whispers:read' |
+	'whispers:edit';
+
 export interface TwitchOptions extends ProviderOptions {
+	scopes?: TWITCH_SCOPES[] | string[];
+
+	/**
+	 * Defaults to `false`. From [Twitch OAuth2 docs](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#get-the-user-to-authorize-your-app):
+	 * 
+	 * > Set to **true** to force the user to re-authorize your app’s access to their resources. The default is **false**.
+	 */
+	force_verify?: boolean;
 }
 
 /**
@@ -27,7 +79,8 @@ export class Twitch implements Provider {
 			client_id: this.options.clientId,
 			redirect_uri: this.redirectUri,
 			response_type: 'code',
-			scope: ''.concat(this.options.scopes?.join(' ') || '').trim()
+			scope: ''.concat(this.options.scopes?.join(' ') || '').trim(),
+			force_verify: this.options.force_verify ?? false,
 		}));
 	}
 
